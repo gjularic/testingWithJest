2fb5e51c3c00349dbb956d1a9d85a9ae73796674
gjularic <gvozdan.jularic@gmail.com>
2022-03-21 20:46:09 +0000

    update

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ test('use jsdom in this test file', () => {
+    const element = document.createElement('div');
+    expect(element).not.toBeNull();
+  });
+
+  const buttonClick = require("../button");
+
+  beforeAll(() => {
+      let fs = require("fs");
+      let fileContents = fs.readFileSync("index.html", "utf-8");
+      document.open();
+      document.write(fileContents);
+      document.close();
+  });
+  
+  describe("DOM tests", () => {
+      test("Expects content to change", () => {
+          buttonClick();
+          expect(document.getElementById("par")
+              .innerHTML).toEqual("You Clicked");
+      });
+      test("h1 should exist", () => {
+          expect(document.getElementsByTagName("h1").length).toBe(1);
+      });
+  });
